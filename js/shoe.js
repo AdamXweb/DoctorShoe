4eea3e73f53a57df3cb6e133585e56eb90b734f9
Adam Kostarelas <adam@trunked.com.au>
2019-08-30 23:42:37 +1000

    feat: WIP everyday shoe. Can change colour by custom adding class

--- js/shoe.js
+++ js/shoe.js
@@ -3,4 +3,10 @@ var prevVal;
         var val = $(this).find('option:selected').val();
         $(".shoe").removeClass(`${prevVal}`).addClass(`${val}`);
         prevVal = val;
-      });
+      })
+var upperVal;
+$("upper").on("change",function(){
+  var uval = $(this).find('option:selected').val();
+  $(".base").removeClass(`${upperVal}`).addClass(`${uval}`);
+  upperVal = uval;
+});
