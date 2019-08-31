1d3eeb6038ab65c09fdca1d936c3ef4a19fc8360
Adam Kostarelas <adam@trunked.com.au>
2019-08-31 12:47:12 +1000

    feat: everyday shoe colours change based on area of shoe

--- js/shoe.js
+++ js/shoe.js
@@ -1,12 +1,30 @@
 var prevVal;
       $("#color").on("change",function(){
-        var val = $(this).find('option:selected').val();
-        $(".shoe").removeClass(`${prevVal}`).addClass(`${val}`);
-        prevVal = val;
-      })
+        var lval = $(this).find('option:selected').val();
+        $(".shoe").removeClass(`${prevVal}`).addClass(`${lval}`);
+        prevVal = lval;
+      });
+var soleVal;
+$("#selectsole").on("change",function(){
+  var soval = $(this).find('option:selected').val();
+  $(".sole").removeClass(`${soleVal}`).addClass(`${soval}`);
+  soleVal = val;
+});
+var laceVal;
+$("#selectlace").on("change",function(){
+  var laval = $(this).find('option:selected').val();
+  $(".lace").removeClass(`${laceVal}`).addClass(`${laval}`);
+  laceVal = laval;
+});
 var upperVal;
-$("upper").on("change",function(){
-  var uval = $(this).find('option:selected').val();
-  $(".base").removeClass(`${upperVal}`).addClass(`${uval}`);
-  upperVal = uval;
+$("#selectupper").on("change",function(){
+  var upval = $(this).find('option:selected').val();
+  $(".decor").removeClass(`${upperVal}`).addClass(`${upval}`);
+  upperVal = upval;
+});
+var baseVal;
+$("#selectbase").on("change",function(){
+  var baval = $(this).find('option:selected').val();
+  $(".base").removeClass(`${baseVal}`).addClass(`${baval}`);
+  baseVal = baval;
 });
