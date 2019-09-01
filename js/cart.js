250e81a4c778931f8f29d84082bb8156370b0562
Adam Kostarelas <adam@trunked.com.au>
2019-09-01 11:00:05 +1000

    fix: WIP add description (options) selected into cart

--- js/cart.js
+++ js/cart.js
@@ -9,10 +9,11 @@ var shoppingCart = (function() {
     cart = [];
     
     // Constructor
-    function Item(name, price, count) {
+    function Item(name, price, count, desc) {
       this.name = name;
       this.price = price;
       this.count = count;
+      this.desc = desc;
     }
     
     // Save cart
@@ -146,9 +147,10 @@ var shoppingCart = (function() {
     var price = Number($(this).data('price'));
     shoppingCart.addItemToCart(name, price, 1);
     $('#evShoe').modal('hide');
+    $('#cart').modal('show');
     displayCart();
   });
-  
+
   // Clear items
   $('.clear-cart').click(function() {
     shoppingCart.clearCart();
@@ -163,6 +165,7 @@ var shoppingCart = (function() {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
         + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td>(" + cartArray[i].desc + ")</td>"
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
