f1ac2e590d8e23fe824417ceee2be7dd7c909bc7
Adam Kostarelas <adam@trunked.com.au>
2019-09-23 21:28:51 +1000

    fix: removed cart addition and sub. Included description WIP

--- js/cart.js
+++ js/cart.js
@@ -166,9 +166,7 @@ var shoppingCart = (function() {
         + "<td>" + cartArray[i].name + "</td>" 
         + "<td>(" + cartArray[i].price + ")</td>"
         + "<td>(" + cartArray[i].desc + ")</td>"
-        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
         + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
         + "<td>" + cartArray[i].total + "</td>" 
@@ -186,21 +184,7 @@ var shoppingCart = (function() {
     shoppingCart.removeItemFromCartAll(name);
     displayCart();
   })
-  
-  
-  // -1
-  $('.show-cart').on("click", ".minus-item", function(event) {
-    var name = $(this).data('name')
-    shoppingCart.removeItemFromCart(name);
-    displayCart();
-  })
-  // +1
-  $('.show-cart').on("click", ".plus-item", function(event) {
-    var name = $(this).data('name')
-    shoppingCart.addItemToCart(name);
-    displayCart();
-  })
-  
+    
   // Item count input
   $('.show-cart').on("change", ".item-count", function(event) {
      var name = $(this).data('name');
