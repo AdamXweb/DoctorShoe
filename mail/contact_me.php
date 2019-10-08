d2ce572ceedcc95a495cf30055139e99851a119f
Adam Kostarelas <adam@trunked.com.au>
2019-10-08 19:01:55 +1100

    feat: added mail sub info and misc fixes.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,32 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_GET['name'])      ||
+   empty($_GET['email'])     ||
+   empty($_GET['phone'])     ||
+   empty($_GET['message'])   ||
+   !filter_var($_GET['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
    }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars($_GET['name']));
+$email_address = strip_tags(htmlspecialchars($_GET['email']));
+$phone = strip_tags(htmlspecialchars($_GET['phone']));
+$message = strip_tags(htmlspecialchars($_GET['message']));
 
-// Create the email and send the message
-$to = 's@student.rmit.edu.au'; //
-$email_subject = "Doctor Shoe new request from:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@rmit.edu.au\n"; // This is the email address the generated message will be from.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+      echo "Name: " . $name. "<br />";
+      echo "Email: " . $email_address. "<br />";
+      echo "Phone: " . $phone. "<br />";
+      echo "Message: " . $message. "<br />";
+
+
+// // Create the email and send the message
+// $to = 's@student.rmit.edu.au'; //
+// $email_subject = "Doctor Shoe new request from:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@rmit.edu.au\n"; // This is the email address the generated message will be from.
+// $headers .= "Reply-To: $email_address";
+// mail($to,$email_subject,$email_body,$headers);
+// return true;
 ?>
